0bd85e5c580796e504478cf1dd3843d9f7505263
tanmayray <tanmayray94@gmail.com>
2025-06-23 02:11:10 +0530

    Solution for two sum problem

---
+++ TwoSum.java
@@ -0,0 +1,67 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+        Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+        2 <= nums.length <= 104
+        -109 <= nums[i] <= 109
+        -109 <= target <= 109
+Only one valid answer exists.
+
+
+        Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        TwoSum app = new TwoSum();
+        System.out.println(Arrays.asList(
+                app.twoSum(new int[]{3,2,4}, 6)
+        ));
+    }
+
+    public Integer[] twoSum(int[] nums, int target) {
+        Integer[] result = new Integer[2];
+
+        Map<Integer, Integer> seen = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(seen.containsKey(nums[i])) {
+                result[0] = seen.get(nums[i]);
+                result[1] = i;
+                return result;
+            }
+            seen.put(target - nums[i], i);
+        }
+
+        return null;
+    }
+}
